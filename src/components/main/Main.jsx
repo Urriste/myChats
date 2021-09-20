daf1473311b607ca2c610a570f4f75449fb1b036
Lucas Urriste <lukeu2002@gmail.com>
2021-09-20 12:02:37 -0300

    added main page, and user data

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import { React, Fragment, useState, useEffect } from "react";
+import "./main.scss";
+import { getAuth } from "firebase/auth";
+import Message from "../message/Message";
+import { useHistory } from "react-router";
 
 const Main = () => {
+  let history = useHistory();
+
+  const [user, setUser] = useState({
+    name: "",
+    photo: "",
+    email: "",
+  });
+
+  const auth = getAuth();
+  console.log(auth);
+
+  useEffect(() => {
+    let { displayName, photoURL, email } = auth.currentUser;
+
+    photoURL = photoURL.replace("s96-c", "s400-c");
+
+    if (displayName) {
+      setUser({
+        name: displayName,
+        photo: photoURL,
+        email: email,
+      });
+    }
+  }, [auth]);
+
   return (
-    <div>
-      <h1>holi</h1>
-    </div>
+    <Fragment>
+      <div className="container-fluid chat-container">
+        <div className="row">
+          <div className="text-area col-sm-12">
+            <Message
+              className="message"
+              userImage={user.photo}
+              name={user.name}
+            ></Message>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import { React, Fragment } from "react";
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <nav className="navbar navbar-dark bg-dark">
+        <span className="navbar-brand mb-0 h1 mx-2">MyChats</span>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Navbar;
