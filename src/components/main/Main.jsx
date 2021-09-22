6464e7cfd86cccd3a79b4a6f0d9c4827643c34e5
Lucas Urriste <lukeu2002@gmail.com>
2021-09-21 22:32:33 -0300

    firebase realtime database added and working, add new styles, delete redux files , update  message layout

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,47 +1,92 @@
 import { React, Fragment, useState, useEffect } from "react";
 import "./main.scss";
 import { getAuth } from "firebase/auth";
+import { getDatabase, ref, push, onValue } from "firebase/database";
 import Message from "../message/Message";
 import { useHistory } from "react-router";
+import sendImg from "../../img/send.png";
 
 const Main = () => {
-  let history = useHistory();
-
-  const [user, setUser] = useState({
-    name: "",
-    photo: "",
-    email: "",
-  });
-
+  //Firebase instances
+  const db = getDatabase();
   const auth = getAuth();
   console.log(auth);
 
-  useEffect(() => {
-    let { displayName, photoURL, email } = auth.currentUser;
+  //States
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([{}]);
 
+  //Handlers and Functions
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleMesssage = (e) => {
+    e.preventDefault();
+    let { displayName, photoURL } = auth.currentUser;
     photoURL = photoURL.replace("s96-c", "s400-c");
 
-    if (displayName) {
-      setUser({
-        name: displayName,
-        photo: photoURL,
-        email: email,
-      });
-    }
-  }, [auth]);
+    let messageObject = {
+      username: displayName,
+      photo: photoURL,
+      message: message,
+    };
+
+    push(ref(db, "messages"), messageObject);
+  };
+
+  const getMessages = () => {
+    const messagesRef = ref(db, "messages");
+    onValue(messagesRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      setMessages(data);
+    });
+  };
+
+  //Effects
+  useEffect(() => {
+    getMessages();
+  }, []);
 
   return (
     <Fragment>
       <div className="container-fluid chat-container">
-        <div className="row">
-          <div className="text-area col-sm-12">
-            <Message
-              className="message"
-              userImage={user.photo}
-              name={user.name}
-            ></Message>
+        <div className="text-area col-sm-12">
+          <div className="chat-welcome__message">
+            <h4 className="welcome-message">¡Bienvenido/a a MyChats!</h4>
+            <p>
+              {" "}
+              <i>Empezá a chatear , mandá un mensaje! </i>{" "}
+            </p>
           </div>
+          {messages &&
+            Object.keys(messages).map((item, index) => {
+              const { username, photo, message } = messages[item];
+
+              return (
+                <Message
+                  userImage={photo}
+                  name={username}
+                  message={message}
+                  key={index}
+                ></Message>
+              );
+            })}
         </div>
+
+        <form className="send-form">
+          <input
+            required
+            onChange={handleChange}
+            type="text"
+            placeholder="Message"
+            className="send-input"
+          />
+          <button className="send-btn" onClick={handleMesssage}>
+            <img src={sendImg} alt="send img" />
+          </button>
+        </form>
       </div>
     </Fragment>
   );
--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,33 +1,16 @@
 import { React, Fragment } from "react";
 import "./message.scss";
 
-const Message = ({ userImage, name }) => {
+const Message = ({ userImage, name, message }) => {
   return (
     <Fragment>
       <div className="message-container container">
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="message container">
-              <div className="row">
-                <div className="user-data col-sm-12">
-                  {" "}
-                  <img
-                    src={userImage}
-                    alt="userimage"
-                    className="message-user__image"
-                  />
-                  <p className="message-user">{name}</p>
-                </div>
-
-                <p className="message-text col-sm-12">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
-                  quod mollitia nihil modi atque doloremque non. Velit quidem
-                  atque reprehenderit. Blanditiis ratione in rerum id, numquam
-                  commodi quo. Quam, optio.
-                </p>
-              </div>
-            </div>
-          </div>
+        <div className="message-left">
+          <img src={userImage} alt="userimage" className="message-photo" />
+        </div>
+        <div className="message-right">
+          <p className="message-username">{name}</p>
+          <p className="message-content">{message}</p>
         </div>
       </div>
     </Fragment>
